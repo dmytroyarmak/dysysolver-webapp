9ee430dfcf353269f69ab384d02584ab3dfc477e
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-06-02 23:56:16 +0300

    Rebuild step 1

--- src/app/dss.controller.js
+++ src/app/dss.controller.js
@@ -10,19 +10,54 @@
   function DssController () {
     var vm = this;
 
+    var STORED_MODEL_G = '(1/6.28)*(log(1/(x^2+t^2)^0.5))/log(2.718)';
+    var STORED_MODEL_U = '-0.2*sin(x/5)-0.25*cos(t/4)';
+    var STORED_MODEL_Y = '5*sin(x/5)+4*cos(t/4)';
+
+    vm.selectedStepIndex = 0;
     vm.problem = {
-      dimension: 1
+      model: {
+        g: '',
+        u: '',
+        y: ''
+      },
+      area: {
+        t: null,
+        x1: null,
+        x2: null
+      }
     };
 
-    vm.processes = [
-      {},
-      {}
-    ];
+    vm.selectStoredModel = selectStoredModel;
+    vm.submitModelStepForm = submitModelStepForm;
+    vm.isAreaStepDisabled = isAreaStepDisabled;
+    vm.isBoundaryConditionsStepDisabled = isBoundaryConditionsStepDisabled;
+    vm.isMVariablesStepDisabled = isMVariablesStepDisabled;
+    vm.isSolutionStepDisabled = isSolutionStepDisabled;
 
-    vm.submitProblem = submitProblem;
+    function selectStoredModel () {
+      vm.problem.model.g = STORED_MODEL_G;
+      vm.problem.model.u = STORED_MODEL_U;
+      vm.problem.model.y = STORED_MODEL_Y;
+    }
 
-    function submitProblem () {
-      alert('42');
+    function submitModelStepForm () {
+      vm.selectedStepIndex = 1;
+    }
+    function isAreaStepDisabled () {
+      return vm.modelStepForm && vm.modelStepForm.$invalid;
+    }
+
+    function isBoundaryConditionsStepDisabled () {
+      return true;
+    }
+
+    function isMVariablesStepDisabled () {
+      return true;
+    }
+
+    function isSolutionStepDisabled () {
+      return true;
     }
   }
 })();
