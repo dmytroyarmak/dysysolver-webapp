c67c6c1c4ebb47e1aa80e26ebac5159b14bb9f25
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-06-03 01:07:46 +0300

    Implement steps 3-4 and mock 5th

--- src/app/dss.controller.js
+++ src/app/dss.controller.js
@@ -25,14 +25,44 @@
         t: null,
         x1: null,
         x2: null
+      },
+      conditions: {
+        initial: [
+          {x: null, y: null},
+          {x: null, y: null}
+        ],
+        boundary: [
+          {x: null, t: null, y: null},
+          {x: null, t: null, y: null}
+        ]
+      },
+      mVariables: {
+        m0: [
+            {x: null, t: null},
+            {x: null, t: null}
+        ],
+        m: [
+            {x: null, t: null},
+            {x: null, t: null}
+        ]
       }
     };
 
     vm.selectStoredModel = selectStoredModel;
+    vm.addInitialCondition = addInitialCondition;
+    vm.addBoundaryCondition = addBoundaryCondition;
+    vm.removeInitialCondition = removeInitialCondition;
+    vm.removeBoundaryCondition = removeBoundaryCondition;
+    vm.addM0Variable = addM0Variable;
+    vm.addMVariable = addMVariable;
+    vm.removeM0Variable = removeM0Variable;
+    vm.removeMVariable = removeMVariable;
     vm.submitModelStepForm = submitModelStepForm;
     vm.submitAreaStepForm = submitAreaStepForm;
+    vm.submitConditionsStepForm = submitConditionsStepForm;
+    vm.submitMVariablesStepForm = submitMVariablesStepForm;
     vm.isAreaStepDisabled = isAreaStepDisabled;
-    vm.isBoundaryConditionsStepDisabled = isBoundaryConditionsStepDisabled;
+    vm.isConditionsStepDisabled = isConditionsStepDisabled;
     vm.isMVariablesStepDisabled = isMVariablesStepDisabled;
     vm.isSolutionStepDisabled = isSolutionStepDisabled;
 
@@ -42,28 +72,96 @@
       vm.problem.model.y = STORED_MODEL_Y;
     }
 
+    function addInitialCondition () {
+      vm.problem.conditions.initial.push({
+        x: null,
+        y: null
+      });
+    }
+
+    function addBoundaryCondition () {
+      vm.problem.conditions.boundary.push({
+        x: null,
+        t: null,
+        y: null
+      });
+    }
+
+    function removeInitialCondition (initialCondition) {
+      _removeElementFromArray(vm.problem.conditions.initial, initialCondition);
+    }
+
+    function removeBoundaryCondition (boundaryCondition) {
+      _removeElementFromArray(vm.problem.conditions.boundary, boundaryCondition);
+    }
+
+    function addM0Variable () {
+      vm.problem.mVariables.m0.push({
+        x: null,
+        t: null
+      });
+    }
+
+    function addMVariable () {
+      vm.problem.mVariables.m.push({
+        x: null,
+        t: null
+      });
+    }
+
+    function removeM0Variable (m0Variable) {
+      _removeElementFromArray(vm.problem.mVariables.m0, m0Variable);
+    }
+
+    function removeMVariable (mVariable) {
+      _removeElementFromArray(vm.problem.mVariables.m, mVariable);
+    }
+
     function submitModelStepForm () {
-      vm.selectedStepIndex = 1;
+      if (!isAreaStepDisabled()) {
+        vm.selectedStepIndex = 1;
+      }
     }
 
     function submitAreaStepForm () {
-      vm.selectedStepIndex = 2;
+      if (!isConditionsStepDisabled()) {
+        vm.selectedStepIndex = 2;
+      }
+    }
+
+    function submitConditionsStepForm () {
+      if (!isMVariablesStepDisabled()) {
+        vm.selectedStepIndex = 3;
+      }
+    }
+
+    function submitMVariablesStepForm () {
+      if (!isSolutionStepDisabled()) {
+        vm.selectedStepIndex = 4;
+      }
     }
 
     function isAreaStepDisabled () {
       return vm.modelStepForm && vm.modelStepForm.$invalid;
     }
 
-    function isBoundaryConditionsStepDisabled () {
+    function isConditionsStepDisabled () {
       return isAreaStepDisabled() || (vm.areaStepForm && vm.areaStepForm.$invalid);
     }
 
     function isMVariablesStepDisabled () {
-      return true;
+      return isConditionsStepDisabled() || (vm.conditionsStepForm && vm.conditionsStepForm.$invalid);
     }
 
     function isSolutionStepDisabled () {
-      return true;
+      return isMVariablesStepDisabled() || (vm.mVariablesStepForm && vm.mVariablesStepForm.$invalid);
+    }
+
+    function _removeElementFromArray (array, element) {
+      var index = array.indexOf(element);
+      if (index > -1) {
+        array.splice(index, 1);
+      }
     }
   }
 })();
